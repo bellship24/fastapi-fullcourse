12a975b87bac0e8b4bf3b966f80cc3e46ea9754d
bellship24 <bellship24@gmail.com>
2021-10-27 22:05:59 +0900

    Add 19. Using Docs Tags & 20. Relationship

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,14 +1,13 @@
+from typing import List
 from pydantic import BaseModel
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
 
 
-class ShowBlog(BaseModel):
-    title: str
-    body: str
-    class Config:
+class Blog(BlogBase):
+    class Config():
         orm_mode = True
 
 
@@ -17,8 +16,19 @@ class User(BaseModel):
     email:str
     password:str
 
+
 class ShowUser(BaseModel):
     name:str
     email:str
+    blogs: List[Blog] = []
     class Config():
+        orm_mode = True
+
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+
+    class Config:
         orm_mode = True

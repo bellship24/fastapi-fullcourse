c35b6e8cccae50fb1a8ab58da1ead93d8d1a4b27
bellship24 <bellship24@gmail.com>
2021-11-08 02:31:33 +0900

    Add 26. Route behind Authentication

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,6 +1,6 @@
 from typing import List
 from fastapi import APIRouter, Depends, status
-import schemas, database, models
+import schemas, database, oauth2
 from sqlalchemy.orm import Session
 from repository import blog
 
@@ -12,25 +12,24 @@ get_db = database.get_db
 
 
 @router.get('/', response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(get_db)):
+def all(db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.get_all(db)
 
-
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create(request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.create(request, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(get_db)):
+def destroy(id, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.destroy(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+def update(id, request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.update(id, request, db)
 
 
 @router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
-def show(id, db: Session = Depends(get_db)):
+def show(id, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.show(id, db)
